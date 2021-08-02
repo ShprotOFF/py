7b2917463b6d7e047fef0f769ed9e1d1222b2d70
ShprotOFF <shprot1984@gmail.com>
2021-08-02 13:06:32 +1000

    Trying it out python

---
+++ mablib.py
@@ -0,0 +1,6 @@
+# MabLib.py
+noun = input("Введите существительное: ")
+abjective = input("Введите прилагательное: ")
+verb = input("Ну и глагол: ")
+print("Твоя чепуха:")
+print("Этот", abjective, noun, verb, "на ленивую рыжую собаку")
